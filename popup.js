86295e358bb6d21a9c9f34535d67e79eb3f53222
jhilaa <julien.hilaire@imelmi.net>
2022-02-03 16:15:50 +0100

    ajout d'une tâche ok

--- popup.js
+++ popup.js
@@ -1,29 +1,51 @@
-let btnNewTask = document.getElementById("btnNewTask");
+let container = document.getElementById("sortable");
+let btnAddTask = document.getElementById("btnAddTask");
+let taskModel = document.getElementById("taskModel");
+let inputTask = document.getElementById("inputTask");
+let inputDescription = document.getElementById("inputDescription");
 
-/*
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-*/
+function addItem(task, description) {
+  let newTask = taskModel.cloneNode(true);
+  newTask.querySelector(".item-author").textContent = task;
+  newTask.querySelector(".item-except").textContent = description;
+  container.appendChild(newTask);
+}
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-btnNewTask.addEventListener("click", async () => {
-  let itemsList = document.getElementsByClassName("list");
-  let listItem = document.getElementsByClassName("list-item");
-  let newTask = listItem[0].cloneNode(true);
-  console.log(newTask.querySelector(".item-author"));
-  newTask.querySelector(".item-author").innerHTML =
-    document.getElementById("txtNewTask").value;
-  newTask.querySelector(".item-except").innerHTML = document.getElementById(
-    "txtNewTaskDescription"
-  ).value;
-  itemsList[0].appendChild(newTask);
-});
+function setDataTasks() {
+  let taskElements = Array.from(document.getElementsByClassName("flex"));
+  tasks = taskElements.map((taskElement) => {
+    let task = taskElement.getElementsByClassName("item-author")[0];
+    let description = taskElement.getElementsByClassName("item-except")[0];
+    return {
+      "task": task.textContent,
+      "description": description.textContent,
+    };
+  });
+  chrome.storage.sync.set(
+    {
+      tasks,
+    },
+    function () {
+      console.log("Recorded");
+    }
+  );
+}
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+function getDataTasks() {
+  let key = "tasks";
+  chrome.storage.sync.get([key], function (data) {
+    let dataTasks = data.tasks;
+    dataTasks.map((task) => addItem(task["task"], task["description"]));
   });
 }
+
+btnAddTask.addEventListener("click", () => {
+  // ajout d'un item dans la liste en clonant un item existant
+  // et on change ses valeurs selon celles des inputs
+  addItem(inputTask.value, inputDescription.value);
+  setDataTasks();
+  //getDataTasks();
+});
+
+// init de la liste des tâches à partir de ce qu'il y  dans le storage
+getDataTasks();
