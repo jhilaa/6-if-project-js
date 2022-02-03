5673d59971fd7addb52e9b147e26e80161444d33
jhilaa <julien.hilaire@imelmi.net>
2022-02-03 21:43:58 +0100

    ajout et suppr task ok

--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 let container = document.getElementById("sortable");
-let btnAddTask = document.getElementById("btnAddTask");
 let taskModel = document.getElementById("taskModel");
 let inputTask = document.getElementById("inputTask");
 let inputDescription = document.getElementById("inputDescription");
@@ -8,10 +7,16 @@ function addItem(task, description) {
   let newTask = taskModel.cloneNode(true);
   newTask.querySelector(".item-author").textContent = task;
   newTask.querySelector(".item-except").textContent = description;
+  console.log(newTask.querySelector(".btnDeleteTask"));
+  newTask.querySelector(".btnDeleteTask").addEventListener("click", (e) => {
+    //console.log("vvv" + task);
+    e.target.closest(".list-item").remove();
+  });
   container.appendChild(newTask);
+  //-----------
 }
 
-function setDataTasks() {
+function storeDataTasks() {
   let taskElements = Array.from(document.getElementsByClassName("flex"));
   tasks = taskElements.map((taskElement) => {
     let task = taskElement.getElementsByClassName("item-author")[0];
@@ -25,27 +30,25 @@ function setDataTasks() {
     {
       tasks,
     },
-    function () {
+    () => {
       console.log("Recorded");
     }
   );
 }
 
 function getDataTasks() {
-  let key = "tasks";
-  chrome.storage.sync.get([key], function (data) {
+  chrome.storage.sync.get(["tasks"], function (data) {
     let dataTasks = data.tasks;
     dataTasks.map((task) => addItem(task["task"], task["description"]));
   });
 }
 
-btnAddTask.addEventListener("click", () => {
-  // ajout d'un item dans la liste en clonant un item existant
-  // et on change ses valeurs selon celles des inputs
-  addItem(inputTask.value, inputDescription.value);
-  setDataTasks();
-  //getDataTasks();
-});
-
+//---------------------------
 // init de la liste des tâches à partir de ce qu'il y  dans le storage
 getDataTasks();
+
+btnAddTask.addEventListener("click", () => {
+  console.log("1");
+  addItem(inputTask.value, inputDescription.value);
+  storeDataTasks();
+});
