c349f5250f471a28333246c2d7ffe66e2211381e
jhilaa <julien.hilaire@imelmi.net>
2022-02-05 20:04:58 +0100

    edit & delete ok. Minor display issues

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,34 +1,63 @@
-let container = document.getElementById("sortable");
+let sortable = document.getElementById("sortable");
 let taskModel = document.getElementsByClassName("list-item-model");
 let inputTask = document.getElementById("inputTask");
 let inputDescription = document.getElementById("inputDescription");
 
 function addItem(task, description) {
   let newTask = taskModel[0].cloneNode(true);
-  newTask.querySelector(".item-author").textContent = task;
-  newTask.querySelector(".item-except").textContent = description;
+  newTask.querySelector(".item-author").value = task;
+  newTask.querySelector(".item-except").value = description;
   newTask.classList.add("list-item");
   newTask.classList.add("list-item-model");
   newTask.style.display = "flex";
-  //console.log(newTask.querySelector(".btnDeleteTask"));
+  //----------------
   newTask.querySelector(".btnDeleteTask").addEventListener("click", (e) => {
     e.target.closest(".list-item").remove();
     storeDataTasks();
   });
-  container.appendChild(newTask);
-  //-----------
+  //----------------
+  newTask.querySelector(".btnCheck").addEventListener("click", (e) => {
+    let cardRoot = e.target.closest(".card");
+    let btnCheck = cardRoot.querySelector(".btnCheck");
+    btnCheck.classList.add("d-none");
+    storeDataTasks();
+  });
+  //-----------------
+  newTask.querySelector(".item-author").addEventListener("input", (e) => {
+    let cardRoot = e.target.closest(".card");
+    let btnCheck = cardRoot.querySelector(".btnCheck");
+    btnCheck.classList.remove("d-none");
+  }),
+    //-----------------
+    newTask.querySelector(".item-except").addEventListener("input", (e) => {
+      let cardRoot = e.target.closest(".card");
+      let btnCheck = cardRoot.querySelector(".btnCheck");
+      btnCheck.style.display = "flex";
+      btnCheck.classList.remove("d-none");
+    }),
+    //------------------
+    sortable.appendChild(newTask);
+}
+
+function hideAllEditButtons() {
+  let allEditButtons = document.getElementsByClassName("btnEditTask");
+  Array.from(allEditButtons).forEach((btn) => {
+    btn.classList.add("d-none");
+  });
 }
 
 function storeDataTasks() {
+  // hideAllEditButtons();
   let taskElements = Array.from(document.getElementsByClassName("list-item"));
   tasks = taskElements.map((taskElement) => {
     let task = taskElement.getElementsByClassName("item-author")[0];
     let description = taskElement.getElementsByClassName("item-except")[0];
     return {
-      "task": task.textContent,
-      "description": description.textContent,
+      "task": task.value,
+      "description": description.value,
     };
   });
+  console.log(tasks);
   chrome.storage.sync.set(
     {
       tasks,
@@ -46,16 +75,11 @@ function getDataTasks() {
   });
 }
 
-function cleanInput() {
-  inputTask.value = "";
-  inputDescription.value = "";
-}
 //---------------------------
 // init de la liste des tâches à partir de ce qu'il y  dans le storage
 getDataTasks();
 
 btnAddTask.addEventListener("click", () => {
-  addItem(inputTask.value, inputDescription.value);
+  addItem("", "");
   storeDataTasks();
-  cleanInput();
 });
